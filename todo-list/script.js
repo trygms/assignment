9c69fe6c65ba3b7744fb1a2affb32cddb0605135
Captain Price <murathoncu@gmail.com>
2022-09-23 19:11:25 +0300

    todo

--- todo-list/script.js
+++ todo-list/script.js
@@ -13,5 +13,44 @@ addBtn.addEventListener("click", () => {
       completed: false,
       text: todoInput.value,
     };
+    createListElement(newTodo);
+    todoInput.value = "";
   }
 });
+
+const createListElement = (newTodo) => {
+  const { id, completed, text } = newTodo;
+
+  const li = document.createElement("li");
+  //   li.id = newTodo.id;
+  li.setAttribute("id", id);
+
+  //   newTodo.completed ? li.classList.add("completed") : "";
+  completed && li.classList.add("checked");
+
+  const okIcon = document.createElement("i");
+  okIcon.setAttribute("class", "fas fa-check");
+  li.appendChild(okIcon);
+
+  const p = document.createElement("p");
+  const pNodeText = document.createTextNode(text);
+  p.appendChild(pNodeText);
+  li.appendChild(p);
+
+  const deleteIcon = document.createElement("i");
+  deleteIcon.setAttribute("class", "fas fa-trash");
+  li.appendChild(deleteIcon);
+
+  console.log(li);
+  todoUl.appendChild(li);
+};
+
+todoInput.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    addBtn.click();
+  }
+});
+
+window.onload = function () {
+  todoInput.focus();
+};
