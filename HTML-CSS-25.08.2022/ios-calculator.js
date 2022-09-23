a3bbc37edecb3375570db726d94906b2a5554505
Captain Price <murathoncu@gmail.com>
2022-09-23 22:58:40 +0300

    ios calculator js

--- HTML-CSS-25.08.2022/ios-calculator.js
+++ HTML-CSS-25.08.2022/ios-calculator.js
@@ -1,5 +1,56 @@
-document.getElementById("number7").addEventListener("click", myFunction);
+let first = "";
+let second = "";
+let sum = 0;
+let sum2 = 1;
 
-function myFunction() {
-  document.getElementById("number7").innerHTML = "YOU CLICKED ME!";
-}
+const screenNow = document.querySelector(".now");
+const screenSum = document.querySelector(".sum");
+
+document.querySelector(".container").addEventListener("click", (e) => {
+  //? Operators
+  function add() {}
+  if (e.target.classList.contains("add")) {
+    second = Number(first);
+    first = "";
+    sum += second;
+    sum2 = sum;
+    screenSum.innerText = sum;
+  }
+  if (e.target.classList.contains("sub")) {
+    second = Number(first);
+    first = "";
+    sum -= second;
+    sum2 = sum;
+    screenSum.innerText = sum;
+  }
+  if (e.target.classList.contains("x")) {
+    second = Number(first);
+    first = "";
+    sum2 *= second;
+    sum = sum2;
+    screenSum.innerText = sum;
+  }
+  if (e.target.classList.contains("division")) {
+    second = Number(first);
+    first = "";
+    sum2 /= second;
+    sum = sum2;
+    screenSum.innerText = sum;
+  }
+
+  //? Numbers
+  if (e.target.classList.contains("number")) {
+    if (e.target.classList.contains("equal")) {
+      screenNow.innerText = sum + Number(first);
+      first = "";
+    } else {
+      first += "" + `${e.target.innerText}`;
+      screenNow.innerText = first;
+    }
+  }
+
+  //? AC
+  if (e.target.classList.contains("ac")) {
+    window.location.reload();
+  }
+});
