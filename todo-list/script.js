b7291cf19edbefe5ebf941a9874f320514266a3f
Captain Price <murathoncu@gmail.com>
2022-09-23 19:48:17 +0300

    localGetItem

--- todo-list/script.js
+++ todo-list/script.js
@@ -4,6 +4,8 @@ const addBtn = document.getElementById("todo-button");
 const todoInput = document.getElementById("todo-input");
 const todoUl = document.getElementById("todo-ul");
 
+let todos = JSON.parse(localStorage.getItem("TODOS")) || [];
+
 addBtn.addEventListener("click", () => {
   if (todoInput.value.trim() === "") {
     alert("Please enter new todo");
@@ -14,6 +16,11 @@ addBtn.addEventListener("click", () => {
       text: todoInput.value,
     };
     createListElement(newTodo);
+
+    todos.push(newTodo);
+    localStorage.setItem("TODOS", JSON.stringify(todos));
+
+    console.log(todos);
     todoInput.value = "";
   }
 });
@@ -41,14 +48,17 @@ const createListElement = (newTodo) => {
   deleteIcon.setAttribute("class", "fas fa-trash");
   li.appendChild(deleteIcon);
 
-  console.log(li);
+  //   console.log(li);
   todoUl.appendChild(li);
 };
 
 todoUl.addEventListener("click", (e) => {
-  console.log(e.target);
+  //   console.log(e.target);
   if (e.target.classList.contains("fa-trash")) {
     e.target.parentElement.remove();
+  }
+  if (e.target.classList.contains("fa-check")) {
+    e.target.parentElement.classList.toggle("checked");
   }
 });
 
@@ -61,3 +71,10 @@ todoInput.addEventListener("keydown", (e) => {
 window.onload = function () {
   todoInput.focus();
 };
+
+const renderSavedTodos = () => {
+  todos.forEach((element) => {
+    createListElement(element);
+  });
+};
+renderSavedTodos();
