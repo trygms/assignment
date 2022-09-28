e6e049aacd8b2286e0e499ed6f45ed1a871f2c11
Captain Price <murathoncu@gmail.com>
2022-09-28 21:01:42 +0300

    first lesson done

--- weather-app-inClass/weather.js
+++ weather-app-inClass/weather.js
@@ -3,4 +3,30 @@ const input = document.querySelector(".container input");
 const msg = document.querySelector("span.msg");
 const list = document.querySelector(".ajax-section .cities");
 
-localStorage.setItem("tokenKey", "4e7961cf4460b44aaf06bfad0d1c68e8");
+localStorage.setItem(
+  "tokenKey",
+  "GC1W7oh9ur4LPh2qcglqTLRww80StDspeZhb9AjvuNDxCU4nmXtenvUd/aBohAFI"
+);
+// localStorage.setItem(
+//   "tokenKeyEncrypted",
+//   EncryptStringAES("4e7961cf4460b44aaf06bfad0d1c68e8")
+// );
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getWeatherDataFromApi();
+});
+
+//? Get Api Func.
+const getWeatherDataFromApi = async () => {
+  //alert("http request is gone!");
+  const tokenKey = DecryptStringAES(localStorage.getItem("tokenKey"));
+  //   alert(tokenKey);
+  const inpputValue = input.value;
+  const units = "metric";
+  const lang = "tr";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inpputValue}&appid=${tokenKey}&units=${units}&lang=${lang}`;
+
+  const response = await fetch(url).then((response) => response.json());
+  console.log(response);
+};
