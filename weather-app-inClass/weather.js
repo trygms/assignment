b70feee7240470d0fb49d7fb32f70c3a85759528
Captain Price <murathoncu@gmail.com>
2022-09-28 20:18:34 +0300

    first commit

---
+++ weather-app-inClass/weather.js
@@ -0,0 +1,6 @@
+const form = document.querySelector("section.top-banner form");
+const input = document.querySelector(".container input");
+const msg = document.querySelector("span.msg");
+const list = document.querySelector(".ajax-section .cities");
+
+localStorage.setItem("tokenKey", "4e7961cf4460b44aaf06bfad0d1c68e8");
