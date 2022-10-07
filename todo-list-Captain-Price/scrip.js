4411a286a4c88b51fc4277f781543f399a848766
Captain Price <murathoncu@gmail.com>
2022-10-07 20:03:37 +0300

    todo

--- todo-list-Captain-Price/scrip.js
+++ todo-list-Captain-Price/scrip.js
@@ -1,6 +1,7 @@
 const todoInput = document.querySelector("#todo-input");
 const todoButton = document.getElementById("todo-button");
 const todoUl = document.querySelector("#todo-ul");
+const todoDesc = document.querySelector("#todo-desc-input");
 
 document.querySelector(".todo-main").addEventListener("click", (event) => {
   //   console.log(event.target.className);
@@ -9,6 +10,7 @@ document.querySelector(".todo-main").addEventListener("click", (event) => {
       id: new Date().getTime(),
       completed: false,
       text: todoInput.value,
+      description: todoDesc.value,
     };
     addNewTodoAtList(todoObj);
     todoInput.value = "";
@@ -18,14 +20,30 @@ document.querySelector(".todo-main").addEventListener("click", (event) => {
 const addNewTodoAtList = (newTodo) => {
   const { id, completed, text } = newTodo;
 
-  //? icerik eklendi
+  //? li eklendi
   const li = document.createElement("li");
-  li.innerText = text;
   todoUl.appendChild(li);
 
   //? id eklendi
   li.setAttribute("id", id);
 
-  //? complete class eklendi
-  li.setAttribute("class", "fas fa-check");
+  //? okIconclass eklendi
+  const okIcon = document.createElement("i");
+  okIcon.setAttribute("class", "fas fa-check");
+  li.appendChild(okIcon);
+
+  //? text p olarak eklendi
+  const p = document.createElement("p");
+  p.innerText = text;
+  li.appendChild(p);
+
+  //? small deneme
+  const sml = document.createElement("small");
+  sml.innerText = "\n price";
+  p.appendChild(sml);
+
+  //? deleteIcon eklendi
+  const dltIcon = document.createElement("i");
+  dltIcon.setAttribute("class", "fas fa-trash");
+  li.appendChild(dltIcon);
 };
